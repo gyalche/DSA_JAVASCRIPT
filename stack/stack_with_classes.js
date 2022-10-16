b87fcca8a110185f516a295373cd90f20db7f043
Dawa <sherpadawa01@outlook.com>
2022-10-16 13:14:22 +0545

    stack with classes

--- stack/stack_with_classes.js
+++ stack/stack_with_classes.js
@@ -1,3 +1,37 @@
+// class Stack {
+//   item = [];
+//   currentSize;
+//   maxSize;
+//   constructor(size) {
+//     this.maxSize = size;
+//     this.currentSize = this.item.length;
+//   }
+
+//   push(newValue) {
+//     if (this.currentSize >= this.maxSize) {
+//       alert('stack is full');
+//     } else {
+//       this.item[this.currentSize] = newValue;
+//       this.currentSize++;
+//     }
+//   }
+//   pop() {
+//     if (this.currentSize > 0) {
+//       this.currentSize--;
+//       this.item.length = this.currentSize;
+//     } else {
+//       alert('Stack is already empty');
+//     }
+//   }
+//   display() {
+//     console.log(this.item);
+//   }
+// }
+
+// st1 = new Stack(5);
+// st1.push(20);
+// st1.display();
+
 class Stack {
   item = [];
   currentSize;
@@ -6,28 +40,24 @@ class Stack {
     this.maxSize = size;
     this.currentSize = this.item.length;
   }
-
-  push(newValue) {
-    if (this.currentSize >= this.maxSize) {
-      alert('stack is full');
-    } else {
-      this.item[this.currentSize] = newValue;
+  push(value) {
+    if (this.currentSize < this.maxSize) {
+      this.item[this.currentSize] = value;
       this.currentSize++;
+    } else {
+      alert('stack exceed');
     }
   }
   pop() {
-    if (this.currentSize > 0) {
+    if (this.currentSize < 0) {
+      alert('Stack is already empty');
+    } else {
       this.currentSize--;
       this.item.length = this.currentSize;
-    } else {
-      alert('Stack is already empty');
     }
   }
   display() {
     console.log(this.item);
   }
 }
-
-st1 = new Stack(5);
-// st1.push(20);
-// st1.display();
+const st = new Stack(5);
