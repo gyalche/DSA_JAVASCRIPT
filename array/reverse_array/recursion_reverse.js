57200d8cb28658a0474385ca5195ec9cd08644c7
Dawa <sherpadawa01@outlook.com>
2022-10-17 10:29:41 +0545

    reverse-array with recursion revision

--- array/reverse_array/recursion_reverse.js
+++ array/reverse_array/recursion_reverse.js
@@ -22,8 +22,17 @@ function customReverse(data, start, end) {
     customReverse(data, start + 1, end - 1);
   }
 }
-
 customReverse(data, 0, data.length - 1);
+// function customReverse(data, start, end) {
+//   if (start <= end) {
+//     temp = data[start];
+//     data[start] = data[end];
+//     data[end] = temp;
+//     customReverse(data, start + 1, end - 1);
+//   }
+// }
+
+// customReverse(data, 0, data.length - 1);
 
 // const rev = data.reverse();
 // console.log(rev);
