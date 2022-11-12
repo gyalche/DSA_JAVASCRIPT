e58f37cff6f92816d6a4986253c73f60334527d3
Dawa <sherpadawa01@outlook.com>
2022-11-12 09:57:21 +0545

    max occuring

--- maxOccuring/max_occuring.js
+++ maxOccuring/max_occuring.js
@@ -3,7 +3,7 @@ let strObj = {};
 let maxKey = '';
 
 for (let i = 0; i < str.length; i++) {
-  console.log(str[i]);
+  //   console.log(strObj[str[i]]);
 
   let key = str[i];
   if (!strObj[key]) {
@@ -16,3 +16,9 @@ for (let i = 0; i < str.length; i++) {
 }
 console.log(strObj);
 console.log('the max key is ' + maxKey);
+
+//type of array is object;
+// let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// let another = data.splice(1, 1, 100);
+// console.log('the length is ' + another);
+// console.log(typeof data);
